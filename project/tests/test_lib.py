c1630d703452d438d26421914207cfc548bffe9f
Cam Edwards <cameron.e@sabermine.ai>
2025-06-06 12:15:09 +0100

    Added more test cases

--- project/tests/test_lib.py
+++ project/tests/test_lib.py
@@ -3,3 +3,7 @@ from project.src.fizzbuzz import fizzbuzz
 def test_fizz():
     result = fizzbuzz(3)
     assert result == "Fizz"
+
+def test_buzz():
+    result = fizzbuzz(5)
+    assert result == "Buzz"
